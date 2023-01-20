fcaf7fb1774e3179d063a3f4ae20fdd7cf315025
Abhiram <abhiram.reddy@shopup.org>
2023-01-20 16:07:29 +0530

    add listener

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const ctx = require("express-http-context");
 const express = require("express");
 const database = require("./middleware/database.middleware");
 const http = require("http");
-const { PrismaClient } = require("@prisma/client");
+// const signup = require("routes/SignUp.js")
 
 const app = express();
 app.use(ctx.middleware);
 app.use(database.addDBToContext);
 
+app.get("/", function (req, res){
+    res.header({
+        "Content-Type": "application/json"
+    })
+    res.send({
+        Status: 200,
+        message: "Welcome to doop backend"
+    })
+})
+
+http.createServer(app).listen(8000, function(){
+    console.log("server listening at 8000");
+})
