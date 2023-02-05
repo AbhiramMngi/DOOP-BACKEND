c0fdb2b30ac3d4467bb9c742b18afbbed1053132
pvenkatt <venkatponnaluri2@gmail.com>
2023-02-05 18:18:09 +0530

    editblog

--- routes/EditBlog.js
+++ routes/EditBlog.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const ctx = require("express-http-context");
-router.post("/new", publishBlog); 
-router.put("/edit/:blogid",editBlog);
-router.delete("/delete/:blogid",deleteBlog);
+const db = require("../database/database").db;
 
-async function publishBlog(req,res,next){
-    const db = ctx.get("db");    
+router.post("/new", publishBlog); 
+router.put("/edit",editBlog);
+router.delete("/delete",deleteBlog);
+
+async function publishBlog(req,res,next){      
     console.log(req.body);
-    const{blogger_id,title,blog_content} = req.body;
+    const{blogger_id,title,subtitle,blog_content} = req.body;
 
     const record = await db.blogger.findFirst({
         where:{
@@ -16,21 +16,23 @@ async function publishBlog(req,res,next){
         }
     });
     if(record == null){
-        res.status(500);
+        res.status(404);
         res.send({
             isError: true,
             message: "user doesnt exist",
-            status: 500
+            status: 404
         });
-        next();
+        return next();
     }
     
     const data = db.blog.create({
         data:{
-            blog_id:blog_id,
-            // blogger_blog.blogger_id: blogger_id, //add blogger_id to current blog
-            title: title,
-            content: blog_content
+            // // blog_id:blog_id,
+            // blogger_id: blogger_id, //add blogger_id to current blog
+            // title: title,
+            // subtitle: subtitle,
+            // content: {}
+            blogger_id:"a0148ea0-24c9-46c6-b532-b08143e580e4", title:"heylo", subtitle:"ab", content: "{ 'text':'ef ef' }"
         }
     });
 
@@ -42,50 +44,51 @@ async function publishBlog(req,res,next){
                 message: "blog uploaded",
                 status: 200
             });
-            next();
+            return next();
         })
         .catch ((err)=>{
+            console.log(err)
             res.status(500);
             res.send({
                 isError: true,
                 message: err,
                 status: 500,
             });
-            next();
+            return next();
         });
     
 }
     
 
-async function editBlog(req,res,next){
-    const db = ctx.get("db");    
+async function editBlog(req,res,next){    
     console.log(req.body);
     const{blog_id,blogger_id} = req.body;
 
     const record = await db.blogger.findFirst({
         where:{
-            blogger_id:blogger_id
-            //validate blog owner
+            blog_id: blog_id,
         }
     });
     if(record == null){
-        res.status(500);
+        res.status(404);
         res.send({
             isError: true,
             message: "user doesnt exist",
-            status: 500
+            status: 404
         });
-        next();
+        return next();
     }
-    
-    const data = db.blog.create({
-        data:{
-            blog_id:blog_id,
-            // blogger_blog.blogger_id: blogger_id, //add blogger_id to current blog
-            title: title,
-            content: blog_content
-        }
-    });
+        
+    const data =  db.blog.update({
+        where: {
+          blog_id: blog_id,
+        },
+        data: {
+          conent: req.body.content,
+          title: req.body.title,
+          subtitle: req.body.subtitle,
+        },
+      });
 
     data
         .then((data)=>{
@@ -95,7 +98,7 @@ async function editBlog(req,res,next){
                 message: "blog updated",
                 status: 200
             });
-            next();
+            return next();
         })
         .catch ((err)=>{
             res.status(500);
@@ -104,13 +107,12 @@ async function editBlog(req,res,next){
                 message: err,
                 status: 500,
             });
-            next();
+            return next();
         });
     
 }
     
-async function deleteBlog(req,res,next){
-    const db = ctx.get("db");    
+async function deleteBlog(req,res,next){      
     console.log(req.body);
     const{blog_id,blogger_id} = req.body;
 
@@ -121,23 +123,21 @@ async function deleteBlog(req,res,next){
         }
     });
     if(record == null){
-        res.status(500);
+        res.status(404);
         res.send({
             isError: true,
             message: "user doesnt exist",
-            status: 500
+            status: 404
         });
-        next();
-    }
-    
-    const data = db.blog.delete({
-        data:{
-            blog_id:blog_id,
-            // blogger_blog.blogger_id: blogger_id, //add blogger_id to current blog
-            title: title,
-            content: blog_content
-        }
-    });
+        return next();
+    } 
+
+    const data =  db.blog.delete({
+        where: {
+          blog_id: blog_id,
+          blogger_id: blogger_id,
+        },
+      });
 
     data
         .then((data)=>{
@@ -147,16 +147,17 @@ async function deleteBlog(req,res,next){
                 message: "blog deleted",
                 status: 200
             });
-            next();
+            return next();
         })
         .catch ((err)=>{
+            console.log(err)
             res.status(500);
             res.send({
                 isError: true,
                 message: err,
                 status: 500,
             });
-            next();
+            return next();
         });
     
 }
